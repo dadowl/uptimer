356ecbbcd8027ddbfdc32636693efb7435235472
DadOwl <me@dadowl.dev>
2022-05-26 20:25:08 +0700

    add json builder

---
+++ src/main/java/dev/dadowl/uptimer/utils/JsonBuilder.kt
@@ -0,0 +1,45 @@
+package dev.dadowl.uptimer.utils
+
+import com.google.gson.JsonObject
+
+class JsonBuilder {
+    private var json = JsonObject()
+
+    fun add(key: String?, value: String?): JsonBuilder {
+        json.addProperty(key, value)
+        return this
+    }
+
+    fun add(key: String?, value: Int): JsonBuilder {
+        json.addProperty(key, value)
+        return this
+    }
+
+    fun add(key: String?, value: Boolean): JsonBuilder {
+        json.addProperty(key, value)
+        return this
+    }
+
+    fun add(key: String?, value: Long): JsonBuilder {
+        json.addProperty(key, value)
+        return this
+    }
+
+    fun add(key: String?, value: JsonObject?): JsonBuilder {
+        json.add(key, value)
+        return this
+    }
+
+    fun add(key: Int, value: JsonObject?): JsonBuilder {
+        json.add(key.toString(), value)
+        return this
+    }
+
+    override fun toString(): String {
+        return json.toString()
+    }
+
+    fun build(): JsonObject {
+        return json
+    }
+}
